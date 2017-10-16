7aed4317bef2daff9b07ad03d7d0f172c67b9fad
krishroopkk <noreply>
2017-10-16 12:58:44 +0000

    Add component listProd

---
+++ ui/list-prod.reel/list-prod.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/list-prod.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ListProd
+ * @extends Component
+ */
+exports.ListProd = Component.specialize(/** @lends ListProd# */ {
+    constructor: {
+        value: function ListProd() {
+            this.super();
+        }
+    }
+});
